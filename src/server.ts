bbb6423aeb24cd5edca8bde8f45aa5df04f19c7b
Yordenis <acorreoso91@gmail.com>
2026-05-28 13:45:49 -0400

    Abrir las puertas y escuchar en TODAS las interfaces de red, incluyendo las que vienen de EasyPanel

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import { app } from "./app";
 import { env } from "./config/env";
 import { logger } from "./utils/logger";
 
-app.listen(env.PORT, () => {
+app.listen(env.PORT, "0.0.0.0", () => {
   logger.info(`AdeTravel backend running on port ${env.PORT}`);
 });
